83dc0a3deb7d40971fefdd2a4765826b3c9cfc3c
salonikareer <129042573+salonikareer@users.noreply.github.com>
2023-03-29 16:34:55 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,40 @@
+
+#type casting
+
+string = "56"
+number = 44
+number_str = int(string)
+addition = number + number_str
+print("the sum of numbers",addition)
+
+
+salary =30000
+bonus= salary * (0.25)
+new_salary = float(salary)
+print("salary after converting to float", new_salary)
+print("salary after adding bonus", new_salary + bonus)
+
+string1 = "01101"
+floating_number = 45.20
+new_str = int(string1)
+new_number = int(floating_number)
+print("String converted into integer: ", new_str)
+print("The floating-point number converted into integer: ", new_number)
+
+
+
+# tuple
+my_tuple = ("mouse", [8, 4, 6], (1, 2, 3))
+print(my_tuple[0])
+
+#list
+numbers = [21, 34, 54, 12]
+print(numbers[2])
+print(numbers[-1])
+
+#dictionary
+numbers = {1: "One", 2: "Two", 3: "Three"}
+print(numbers)
+
+
+
